da51d39d98b68639b9cd06d11d6e6c403a21ce46
Matheus Washington <matheus.uox@gmail.com>
2026-07-01 11:09:50 -0300

    Remove space from the file name

--- # EXERCICIO 019.py
+++ # EXERCICIO 019.py
@@ -1,4 +1,4 @@
-# EXERCICIO 019
+#EXERCICIO 019
 #  Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
 # Faça um programa que ajude ele, lendo o nome deles e escrevendo o do escolhido.
 import random
